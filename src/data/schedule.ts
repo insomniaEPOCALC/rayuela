1984af7c31940b028b6423cfececc2769139a464
insomniaEPOCALC <pi-opal@outlook.jp>
2026-04-16 19:28:44 +0900

    docs: adjust documentation

--- src/data/schedule.ts
+++ src/data/schedule.ts
@@ -9,12 +9,12 @@ export type Schedule = {
 
 export const schedules: Schedule[] = [
   {
-    datetime: "2026-05-14T18:30:00+09:00",
+    datetime: "2026-05-14T19:00:00",
     title: "タイトル未定 @東高円寺UFO CLUB",
   },
   {
-    datetime: "2026-05-24T18:30:00+09:00",
-    title: "その他もろもろの側から vol.1 @三鷹おんがくのじかん",
+    datetime: "2026-05-24T18:30:00",
+    title: "その他もろもろの側から そのいち @三鷹おんがくのじかん",
     ticket: "ADV ¥3,000 / DOOR ¥3,500",
     flyerPath: "../flyers/sonomoro.png",
     details: "石蹴り遊びの主催する、不定期イベント第一弾です。 \r\r 出演：石蹴り遊び / 澁谷浩次 / 零進法 accoustic set / 狩生健志 / 石原碧",
