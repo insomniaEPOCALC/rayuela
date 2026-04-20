58586bc69a33136ac92e9c51b4eb1e9e4d77d089
insomniaEPOCALC <pi-opal@outlook.jp>
2026-04-20 21:20:17 +0900

    adjust news comment

--- src/data/news.ts
+++ src/data/news.ts
@@ -10,13 +10,13 @@ export const news: News[] = [
   {
     datetime: "2026-04-20T21:00:00",
     title: "5月のライブ情報を更新しました",
-    details: "5月のライブ情報を更新しました。今後も随時更新していきますので、ぜひチェックしてください。←github copilotによる自動生成文",
+    details: "5月のライブ情報を更新しました。今後も随時更新していきますので、ぜひチェックしてください。\r ↑github copilotによる自動生成文",
     link: "https://rayuela.jp/schedule",
   },
   {
     datetime: "2026-04-20T21:00:00",
     title: "公式サイトを作成しました",
     imgPath: "/rayuela-test.jpg",
-    details: "公式サイトを作成しました。今後はライブ情報やニュースなどをこちらでお知らせしていきます。←github copilotによる自動生成文",
+    details: "公式サイトを作成しました。今後はライブ情報やニュースなどをこちらでお知らせしていきます。\r ↑github copilotによる自動生成文",
   },
 ];
