157972526c2eda420ed3abb7af289ff43e9dd9ce
insomniaEPOCALC <pi-opal@outlook.jp>
2026-04-20 21:12:45 +0900

    add comment out

--- src/data/schedule.ts
+++ src/data/schedule.ts
@@ -8,13 +8,13 @@ export type Schedule = {
 };
 
 export const schedules: Schedule[] = [
-  {
-    datetime: "2026-05-24T18:30:00",
-    title: "その他もろもろの側から そのいち @三鷹おんがくのじかん",
-    ticket: "ADV ¥3,000 / DOOR ¥3,500",
-    flyerPath: "../flyers/sonomoro.png",
-    details: "石蹴り遊びの主催する、不定期イベント第一弾です。 \r\r 出演：石蹴り遊び / 澁谷浩次 / 零進法 accoustic set / 狩生健志 / 石原碧",
-  },
+  // {
+  //   datetime: "2026-05-24T18:30:00",
+  //   title: "その他もろもろの側から そのいち @三鷹おんがくのじかん",
+  //   ticket: "ADV ¥3,000 / DOOR ¥3,500",
+  //   flyerPath: "../flyers/sonomoro.png",
+  //   details: "石蹴り遊びの主催する、不定期イベント第一弾です。 \r\r 出演：石蹴り遊び / 澁谷浩次 / 零進法 accoustic set / 狩生健志 / 石原碧",
+  // },
   {
     datetime: "2026-05-14T19:00:00",
     title: "Love Love Love @東高円寺UFO CLUB",
